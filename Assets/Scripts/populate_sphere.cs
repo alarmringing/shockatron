56977790823a72a7ebae2ea6692be7dcc19ee3ef
Heather Hall <hhall@stanford.edu>
2016-02-25 10:20:19 -0800

    Fixed Surface Normal Shader

--- Assets/Scripts/populate_sphere.cs
+++ Assets/Scripts/populate_sphere.cs
@@ -8,12 +8,12 @@ public class populate_sphere : MonoBehaviour {
 
 	public void Start () {
 
-		int building_number = 1000;
+		int building_number = 500;
 
 		for (int i = 0; i < building_number; i++) {
 			
 			// determine rotation and position of sphere
-			Vector3 spawnPosition = Random.onUnitSphere * ((planet.transform.localScale.x/2) + building.transform.localScale.y * 0.5f) + planet.transform.position;
+			Vector3 spawnPosition = Random.onUnitSphere * ((planet.transform.localScale.x/2) + building.transform.localScale.y * 0.3f) + planet.transform.position;
 			Quaternion spawnRotation = Quaternion.identity;
 
 			// initatiate the object
@@ -22,6 +22,13 @@ public class populate_sphere : MonoBehaviour {
 			// transform the object
 			newBuiding.transform.LookAt(planet.transform);
 			newBuiding.transform.Rotate(-90, 0, 0);
+
+
+			// randomly scale the size of the object
+			float width_scale =  Random.Range(-2F, 2F);
+			float height_scale = Random.Range (-2F, 2F);
+			newBuiding.transform.localScale += new Vector3(width_scale,height_scale  ,width_scale);
+			//newBuiding.transform.position -= new Vector3 (0, height_scale+1, 0);
 		}
 
 
